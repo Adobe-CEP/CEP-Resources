5cb2a6d7930c151b2ea034a62713de00f847b8a0
neraj <neraj@adobe.com>
2019-11-04 18:25:37 +0530

    updates for Max

--- CEP_9.x/Vulcan.js
+++ CEP_9.x/Vulcan.js
@@ -1,7 +1,7 @@
 /**************************************************************************************************
 *
 * ADOBE SYSTEMS INCORPORATED
-* Copyright 2013 Adobe Systems Incorporated
+* Copyright 2018 Adobe Systems Incorporated
 * All Rights Reserved.
 *
 * NOTICE:  Adobe permits you to use, modify, and distribute this file in accordance with the
