1e72f147f02d02fb94954d564c5bde5299cdf4a6
sujai <57343729+sujaisivanandan@users.noreply.github.com>
2021-06-11 14:44:13 +0530

    Update CSInterface.js
    Update version to 11

--- CEP_11.x/CSInterface.js
+++ CEP_11.x/CSInterface.js
@@ -11,7 +11,7 @@
 *
 **************************************************************************************************/
 
-/** CSInterface - v10.0.0 */
+/** CSInterface - v11.0.0 */
 
 /**
  * Stores constants for the window types supported by the CSXS infrastructure.
